6634ec92374086862e9584c55c934311be438069
Feno <Feno>
2016-04-13 22:24:09 -0300

    Comienzo del proyecto

---
+++ RedMovil-01/app/src/main/java/redmovil/com/fuentes/Main.java
@@ -0,0 +1,13 @@
+package redmovil.com.fuentes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
